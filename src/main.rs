cf97f109acf7d3eb7f6626b0768bd01ae4769d66
forfd8960 <forfd8960@gmail.com>
2024-05-23 07:40:07 +0800

    dep: add eframe and egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+use eframe::NativeOptions;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([660.0, 800.0]),
+        ..Default::default()
+    };
+
+    // Our application state:
+    let mut name = "Requester".to_owned();
+    let mut age = 42;
+
+    eframe::run_simple_native("Send Request App", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Requester");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut name)
+                    .labelled_by(name_label.id);
+            });
+
+            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+
+            if ui.button("Increment").clicked() {
+                age += 1;
+            }
+
+            ui.label(format!("Hello '{name}', age {age}"));
+        });
+    })
 }
